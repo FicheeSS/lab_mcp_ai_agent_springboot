0f454528fc835475e3e106fbda2ac51b073ced9f
theo <theo@syngate.fr>
2026-01-28 15:00:41 +0100

    add spring initializr in agent

---
+++ agent/src/test/java/com/theobocquet/mcp_agent/McpAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theobocquet.mcp_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
